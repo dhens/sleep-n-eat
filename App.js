0881d3a1d186a6d27afdf5d1792ee222155759ca
Natasha Wolfe <natashawolfe@natashas-mbp.lan>
2020-10-22 16:49:41 -0700

    set up tailwind css, set example classes on app.js

--- App.js
+++ App.js
@@ -2,21 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import StandardButton from './src/components/StandardButton';
+import tailwind from 'tailwind-rn'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tailwind('bg-blue-500 h-full')}>
       <StandardButton/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
